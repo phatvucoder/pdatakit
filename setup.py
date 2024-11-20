1ae23889fe77b7e79898a5952d27c7d7f0f3142f
Hoang Phat Vu <vhphatdz@gmail.com>
2024-11-21 00:05:47 +0700

    publish lib

--- setup.py
+++ setup.py
@@ -1,1 +1,29 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='pdatakit',
+    version='0.0.1',
+    author='Hoang-Phat Vu',
+    author_email='phatvucoder@gmail.com',
+    description='A Python library for managing and processing datasets for machine learning workflows.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/phatvucoder/pdatakit',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'matplotlib',
+        'iterative-stratification',
+        'pillow',
+        'pyyaml',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+)
